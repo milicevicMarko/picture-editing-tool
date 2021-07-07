0b53247180c46df3bceda26182f0704b35d5e16d
Marko Milicevic <markomilicevic97@gmail.com>
2021-07-07 17:30:05 +0200

    Changed default button to cancel and additional cleanup

--- src/main/scala/frontend/exit/ExitController.scala
+++ src/main/scala/frontend/exit/ExitController.scala
@@ -1,10 +1,11 @@
 package frontend.exit
 
+import javafx.scene.control
 import scalafx.scene.control.{Alert, ButtonType}
 import scalafx.scene.control.Alert.AlertType
 import scalafx.stage.{Stage, WindowEvent}
 
-object Labels {
+trait ExitController{
   val SaveText: String = "Save"
   val SaveAsText: String = "Save As..."
   val CancelText: String = "Cancel"
@@ -13,34 +14,42 @@ object Labels {
   val AlertTitle: String = "Close Confirmation"
   val AlertHeader: String = "Are you sure you want to exit without saving?"
   val AlertContent: String = "All the work done since your last save will be lost."
+
+  val SaveButtonType = new ButtonType(SaveText)
+  val SaveAsButtonType = new ButtonType(SaveAsText)
+
+  val ButtonTypes = Seq(SaveButtonType, SaveAsButtonType, ButtonType.OK, ButtonType.Cancel)
 }
 
-object ExitController {
-
-  val ButtonTypes = Seq(
-    new ButtonType(Labels.SaveText), new ButtonType(Labels.SaveAsText), ButtonType.OK, ButtonType.Cancel)
+object ExitController extends ExitController {
 
   def fireEvent(stage: Option[Stage]): Unit = stage match {
     case Some(s) => s.fireEvent(new WindowEvent(s, WindowEvent.WindowCloseRequest))
     case None => throw new UnknownError
   }
 
-  def handleExitEvent(stage: Stage, event: WindowEvent): Unit = ExitController.exit(stage) match {
-    case Labels.SaveText => println("Save"); event.consume()
-    case Labels.SaveAsText => println("Save As..."); event.consume()
-    case Labels.CancelText => println("Cancel"); event.consume()
-    case Labels.OkText => println("Exit")
+  def handleExitEvent(stage: Stage, event: WindowEvent): Unit = exit(stage) match {
+    case SaveText => println(SaveText); event.consume()
+    case SaveAsText => println(SaveAsText); event.consume()
+    case CancelText => println(CancelText); event.consume()
+    case OkText => println("Exit")
     case _ => throw new UnknownError
   }
 
-  def initAlert(stage: Stage): Alert = new Alert(AlertType.Warning) {
-    initOwner(stage)
-    title = Labels.AlertTitle
-    headerText = Labels.AlertHeader
-    contentText = Labels.AlertContent
-    // todo - change default focus to cancel
-    buttonTypes = ButtonTypes
-  }
-
   def exit(stage: Stage): String = initAlert(stage).showAndWait().getOrElse(throw new IllegalArgumentException).text
+
+  def initAlert(stage: Stage): Alert = new Alert(AlertType.Warning) {
+    def initDefaultButton(alert: Alert): Unit = {
+      alert.getDialogPane.lookupButton(ButtonType.OK).asInstanceOf[control.Button].setDefaultButton(false)
+      alert.getDialogPane.lookupButton(ButtonType.Cancel).asInstanceOf[control.Button].setDefaultButton(true)
+    }
+
+    initOwner(stage)
+    title = AlertTitle
+    headerText = AlertHeader
+    contentText = AlertContent
+    buttonTypes = ButtonTypes
+
+    initDefaultButton(this)
+  }
 }
